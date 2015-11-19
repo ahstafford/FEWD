59481e7c85b323593fbd908f1f21365dd5f22231
ahstafford <langeah@gmail.com>
2015-11-18 20:49:21 -0500

    Added new pages to final project
    Also added more magic

--- Final Project/js/index.js
+++ Final Project/js/index.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
 
-	//Event to list for button click to show text
+	//Event to listen for button click to show text
 	$('#learnMoreButton').click(showText);
 
-	//funtion to show hidden text
+	//funtion to show hidden text with Learn More button
 	function showText(){
 		$('#learnMoreText').show('slow');
 		$('#learnMoreButton').hide();
 	}
+
+	//Event to listen for section expand
+	$('.serviceTabs h3').click(showTab);
+
+	//funtion to show hidden text on page sections
+	function showTab() {
+		console.log(this);
+		$(this).next('div').slideToggle('slow');
+	}
+
 
 	//Load event to animate border
 	$('.lineGrowBefore').addClass('lineGrowAfter');
