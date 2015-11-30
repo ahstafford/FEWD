babc4063e8f70c67a9cb8a12fa9060734bb3fc2e
ahstafford <langeah@gmail.com>
2015-11-29 20:19:53 -0500

    Added projects and media queries for mobile

--- Final Project/js/index.js
+++ Final Project/js/index.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
 
+	//Load event to animate border
+	$('.lineGrowBefore').addClass('lineGrowAfter');
+
 	//Event to listen for button click to show text
 	$('#learnMoreButton').click(showText);
 
@@ -18,7 +21,55 @@ $(document).ready(function(){
 		$(this).next('div').slideToggle('slow');
 	}
 
+	//Event to listen for mouse over on class Rostech
+	$('.rostech').hover(mouseOverRostech, mouseLeaveRostech);
 
-	//Load event to animate border
-	$('.lineGrowBefore').addClass('lineGrowAfter');
+	//function to show text for Rostech
+	function mouseOverRostech() {
+		$('#rostechText').show();
+	}
+
+	//function to hide text for Rostech
+	function mouseLeaveRostech(){
+		$('#rostechText').hide();
+	}
+
+	//Event to listen for mouse over on class Iron
+	$('.iron').hover(mouseOverIron, mouseLeaveIron);
+
+	//function to show text for Iron
+	function mouseOverIron() {
+		$('#ironText').show();
+	}
+
+	//function to hide text for Iron
+	function mouseLeaveIron(){
+		$('#ironText').hide();
+	}
+
+	//Event to listen for mouse over on class taptin
+	$('.taptin').hover(mouseOverTaptin, mouseLeaveTaptin);
+
+	//function to show text for taptin
+	function mouseOverTaptin() {
+		$('#taptinText').show();
+	}
+
+	//function to hide text for taptin
+	function mouseLeaveTaptin(){
+		$('#taptinText').hide();
+	}
+
+	//Event to listen for mouse over on class utility
+	$('.utility').hover(mouseOverUtility, mouseLeaveUtility);
+
+	//function to show text for utility
+	function mouseOverUtility() {
+		$('#utilityText').show();
+	}
+
+	//function to hide text for utility
+	function mouseLeaveUtility(){
+		$('#utilityText').hide();
+	}
 });
